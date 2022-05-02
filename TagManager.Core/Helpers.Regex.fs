3d6ea92fdcaea6833402d289ea8028d2e240b5ef
Lucien "Mingxi" Du <david.xelnaga@gmail.com>
2022-05-02 16:38:51 -0700

    Add project files.

---
+++ TagManager.Core/Helpers.Regex.fs
@@ -0,0 +1,14 @@
+﻿module TagManager.Helpers.Regex
+
+open System.Text.RegularExpressions
+let getMatchedGroups pattern str =
+    let ``match`` = Regex.Match(pattern, str)
+    if ``match``.Success then
+        ``match``.Groups
+        |> Seq.skip 1
+        |> Seq.map (fun group -> group.Value)
+        |>Some
+    else
+        None
+
+let (|Match|_|) = getMatchedGroups
---
+++ TagManager.Core/TagManager.fs
@@ -0,0 +1,15 @@
+﻿module TagManager.Core
+
+open TagManager.Helpers
+
+let parseTags dir =
+    let tagStringPattern = """^(【.+】)+"""
+    let pattern = """【(?:\d+-)?(.+?)】"""
+
+    match dir with
+    |Regex.Match tagStringPattern groups ->
+        groups
+        |>Seq.choose (Regex.getMatchedGroups pattern)
+        |>Seq.collect id
+        |>Seq.distinct
+    |_ -> Seq.empty
