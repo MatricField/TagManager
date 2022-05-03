d16e94b0a8fdb1a42c788766b3ecd1ab09fc7876
Lucien "Mingxi" Du <david.xelnaga@gmail.com>
2022-05-02 17:06:32 -0700

    add basic cmdlet for testing

---
+++ TagManager.PS/Library.fs
@@ -0,0 +1,18 @@
+﻿module TagManager.PS
+
+open System.Management.Automation
+
+[<Cmdlet(VerbsCommon.Get, "Tag")>]
+type GetTagCommand() =
+    inherit PSCmdlet()
+
+    [<Parameter(
+        Position=0,
+        ValueFromPipeline=true,
+        ValueFromPipelineByPropertyName=true)>]
+    [<ValidateNotNullOrEmpty>]
+    member val Name = "" with get, set
+
+    override this.ProcessRecord() =
+        let tags = Core.parseTags this.Name
+        this.WriteObject(tags, false)
